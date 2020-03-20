2c81cd2eb7a48d4d44ea253eb67c684a078bf3b3
aar.56aint <aaron.hays_7@yahoo.com>
2020-03-20 12:20:18 +0000

    #2 ppts

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 README
+
+Interact with this project by:
+1. git clone git@github.com:56aint/virtual-pet.git
+2. node pet.js
--- _test_/pet.test.js
+++ _test_/pet.test.js
@@ -1,11 +1,26 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-  it('sets the name property', () => {
-    const pet = new Pet('Fido');
-    expect(pet.name).toEqual('Fido');
-    
-    const romy = new Pet('Romy');
-    expect(romy.name).toEqual('Romy');
+  it('returns instance of an object', () => {
+    expect(new Pet('Romy')).toBeInstanceOf(Object);
   });
+  
+  it('sets the name property', () => {
+    const pet = new Pet('Romy');
+    expect(pet.name).toEqual('Romy');
+    
+    //const romy = new Pet('Romy');
+    //expect(romy.name).toEqual('Romy');
+  });
+
+  it('sets the initial age', () => {
+    const pet = new Pet('Romy');
+    expect(pet.age).toBe(0);
+    });
+
+    it('sets the growth in increament of 1', () => {
+      const pet = new Pet('Romy');
+      pet.growUp();
+      expect(pet.age).toBe(1);
+    });
 });
