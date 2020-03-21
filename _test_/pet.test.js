d066ffbaebad1268fd17484163bdb41e669e51bd
aar.56aint <aaron.hays_7@yahoo.com>
2020-03-21 23:30:00 +0000

    #2 added sound method

--- _test_/pet.test.js
+++ _test_/pet.test.js
@@ -6,21 +6,21 @@ describe('constructor', () => {
   });
   
   it('sets the name property', () => {
-    const pet = new Pet('Romy');
-    expect(pet.name).toEqual('Romy');
+    const romy = new Pet('Romy');
+    expect(romy.name).toEqual('Romy');
     
     //const romy = new Pet('Romy');
     //expect(romy.name).toEqual('Romy');
   });
 
   it('sets the initial age', () => {
-    const pet = new Pet('Romy');
-    expect(pet.age).toBe(0);
+    const romy = new Pet('Romy');
+    expect(romy.age).toBe(0);
     });
 
     it('sets the growth in increament of 1', () => {
-      const pet = new Pet('Romy');
-      pet.growUp();
-      expect(pet.age).toBe(1);
+      const romy = new Pet('Romy');
+      romy.growUp();
+      expect(romy.age).toBe(1);
     });
 });
--- src/pet.js
+++ src/pet.js
@@ -4,20 +4,22 @@ class Pet {
   constructor(name) {
     this.name = name;
     this.age = INITIAL_AGE;
-    this.sound_1 = 'Meow';
-    //this.relaxed = 'Meow';
+    //this.sound_1 = 'Meow';
+    this.sound = function (soundString) {
+      return soundString ? soundString : 'ERROR! PLEASE, I NEED A SOUND'
+    };
   }
 
   growUp () {
-    console.log(`My name is ${this.name}, I am ${this.age += 1} now and I can ${this.sound_1} when I am relaxed.` );
+    console.log (`My name is ${this.name}, I am ${this.age += 1} now and I can ${this.sound('Meow')} when I am relaxed.`);
   };
 
 
 }
 
-const pet = new Pet('Romy');
-pet.name
-pet.growUp();
+const romy = new Pet('Romy');
+romy.name
+romy.growUp();
 //const romy = new Pet('Romy');
 
 
