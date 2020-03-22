a367882ece10358e299bb0ccd20a4bf25fc7f1da
aar.56aint <aaron.hays_7@yahoo.com>
2020-03-22 12:39:33 +0000

    #5 pptys, untidy

--- _test_/pet.test.js
+++ _test_/pet.test.js
@@ -22,5 +22,73 @@ describe('constructor', () => {
       const romy = new Pet('Romy');
       romy.growUp();
       expect(romy.age).toBe(1);
+      expect(romy.hunger).toBe(5);
+      expect(romy.fitness).toBe(7);
     });
+
+    xit('increases fitness by 4', () => {
+      const romy = new Pet('Romy');
+  
+      romy.fitness = 4;
+      romy.walk();
+  
+      expect(romy.fitness).toEqual(8);
+    });
+
+    it('increases fitness by 8 to a maximum of 10', () => {
+      const romy = new Pet('Romy');
+  
+      romy.fitness = 8;
+      romy.walk();
+  
+      expect(romy.fitness).toEqual(10);
+    });
+
+    it('decreases hunger level by 3', () => {
+      const romy = new Pet('Romy');
+
+      romy.hunger = 3;
+      romy.feed();
+
+      expect(romy.hunger).toEqual(0);
+    });
+
+    it('to check if romy needs a walk', () => {
+      const romy = new Pet('Romy');
+
+      romy.fitness = 3;
+      romy.checkUp();
+
+      expect(romy.checkUp()).toBe('I need a walk!');
+    });
+
+    it('to check if romy needs to eat', () => {
+      const romy = new Pet('Romy');
+
+      romy.hunger = 5;
+
+      expect(romy.checkUp()).toBe('I am hungry!');
+    });
+
+    it('to check if romy is hungry and needs a walk', () => {
+      const romy = new Pet('Romy');
+
+      romy.hunger = 5;
+      romy.fitness = 3;
+      
+      expect(romy.checkUp()).toBe('I am hungry AND I need a walk!');
+    });
+
+    it('to check if romy is neither hungry nor needs a walk', () => {
+      const romy = new Pet('Romy');
+
+      romy.hunger = 0;
+      romy.fitness = 10;
+
+      expect(romy.checkUp()).toBe('I feel great!');
+    });
+
+
+
+
 });
--- src/pet.js
+++ src/pet.js
@@ -1,9 +1,15 @@
 const INITIAL_AGE = 0;
+const INITIAL_HUNGER_LEVEL = 0;
+const MINIMUM_HUNGER_LEVEL = 0;
+const INITIAL_FITNESS_LEVEL = 10;
+const MAXIMUM_FITNESS_LEVEL = 10;
 
 class Pet {  
   constructor(name) {
     this.name = name;
     this.age = INITIAL_AGE;
+    this.hunger = INITIAL_HUNGER_LEVEL;
+    this.fitness = INITIAL_FITNESS_LEVEL;
     //this.sound_1 = 'Meow';
     this.sound = function (soundString) {
       return soundString ? soundString : 'ERROR! PLEASE, I NEED A SOUND'
@@ -11,15 +17,46 @@ class Pet {
   }
 
   growUp () {
-    console.log (`My name is ${this.name}, I am ${this.age += 1} now and I can ${this.sound('Meow')} when I am relaxed.`);
+    this.hunger += 5; 
+    this.fitness -= 3;
+    console.log (`My name is ${this.name}, I am ${this.age += 1} now and I can ${this.sound('Meow')} when I am relaxed. And as i grow older, i feel less fit and hungrier`);
   };
 
+  walk () {
+    this.fitness += 4;
+    if ((this.fitness + 4) <= MAXIMUM_FITNESS_LEVEL) {
+      this.fitness += 4;
+    } else {
+      this.fitness = MAXIMUM_FITNESS_LEVEL;
+    }
+  };
+
+  feed () {
+    this.hunger -= 3;
+    if((this.hunger - 3 >= INITIAL_HUNGER_LEVEL)) {
+      this.hunger -= 3;
+    }else{
+      this.hunger = INITIAL_HUNGER_LEVEL;
+    }
+  };
+
+  checkUp () {
+    if (this.fitness <= 3 && this.hunger >= 5) {
+      return 'I am hungry AND I need a walk!';
+    }else if (this.fitness <= 3) {
+      return 'I need a walk!';
+    }else if (this.hunger >= 5) {
+      return 'I am hungry!';
+    } else {
+      return 'I feel great!';
+    }
+  };
 
 }
 
 const romy = new Pet('Romy');
-romy.name
-romy.growUp();
+//romy.name
+//romy.growUp();
 //const romy = new Pet('Romy');
 
 
